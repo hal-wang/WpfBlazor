ddc5795c12761dc803aef48b5bf9a8500746c3ba
hal-wang <hi@hal.wang>
2024-09-07 15:52:16 +0800

    init

---
+++ uno.config.mjs
@@ -0,0 +1,9 @@
+﻿// uno.config.mjs
+import { defineConfig, presetUno } from "unocss";
+
+export default defineConfig({
+    content: {
+        filesystem: ["**/*.{html,js,ts,jsx,tsx,razor,cshtml}"],
+    },
+    presets: [presetUno()],
+});
